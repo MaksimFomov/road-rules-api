3a934772ea659ffb68f8b8d5d723e7a7135dde69
Maxim Fomov <maksimfomov26@gmail.com>
2025-02-20 18:53:13 +0300

    init: Created project

---
+++ src/main/java/com/fomov/road_rules_api/RoadRulesApiApplication.java
@@ -0,0 +1,13 @@
+package com.fomov.road_rules_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadRulesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoadRulesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fomov/road_rules_api/RoadRulesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fomov.road_rules_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadRulesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
